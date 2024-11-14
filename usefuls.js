f9f31044910ed8120b7f2fc2fc11cdb495ce8932
Mike <mikhail-gankin@yandex.ru>
2024-11-14 16:12:18 +0300

    +++ Ready for Alpha test

--- usefuls.js
+++ usefuls.js
@@ -1,21 +1,3 @@
-export function getRandomElement(array) {
-	if (array.length === 0) {
-		return null; // Возвращаем null, если массив пуст
-	}
-
-	// Создаем копию массива, чтобы не изменять оригинал
-	const shuffledArray = [...array];
-
-	// Перемешиваем элементы массива
-	for (let i = shuffledArray.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-	}
-
-	// Возвращаем следующий элемент из перемешанного массива
-	return shuffledArray.shift();
-}
-
 export function setAttributeObject(element, attributeName, object) {
 	const objectString = JSON.stringify(object);
 	element.setAttribute(attributeName, objectString);
